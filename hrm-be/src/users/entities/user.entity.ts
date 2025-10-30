13f5dc28221eea56090e0a8801ea1af9200244dc
Nguyen Anh Duc <anhduc20053110@gmail.com>
2025-10-30 11:49:10 +0700

    feat: connect db & create schema user

--- hrm-be/src/users/entities/user.entity.ts
+++ hrm-be/src/users/entities/user.entity.ts
@@ -1,1 +1,62 @@
-export class User {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema({ timestamps: true })
+export class User {
+  @Prop()
+  name: string;
+  @Prop()
+  email: string;
+
+  @Prop()
+  password: string;
+
+  @Prop()
+  age: number;
+
+  @Prop()
+  gender: string;
+
+  @Prop()
+  address: string;
+
+  @Prop({ type: Object })
+  company: {
+    _id: string;
+    name: string;
+  };
+
+  @Prop()
+  refreshToken: string;
+
+  @Prop()
+  createdAt: Date;
+
+  @Prop()
+  updatedAt: Date;
+
+  @Prop()
+  isDeleted: boolean;
+
+  @Prop({ type: Object })
+  createdBy: {
+    _id: mongoose.Types.ObjectId;
+    name: string;
+  };
+
+  @Prop({ type: Object })
+  updatedBy: {
+    _id: mongoose.Types.ObjectId;
+    name: string;
+  };
+
+  @Prop({ type: Object })
+  deletedBy: {
+    _id: mongoose.Types.ObjectId;
+    name: string;
+  };
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
